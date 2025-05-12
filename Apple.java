b25a44cb4e1987245b76b6250d170a421cd19f78
Jennifer Zheng <zjy258085@gmail.com>
2025-05-12 14:23:20 -0400

    add title screen

--- Apple.java
+++ Apple.java
@@ -27,7 +27,7 @@ public class Apple extends Actor
         MyWorld world= (MyWorld) getWorld();
         if(getY() >= world.getHeight())
         {
-            //world.gameOver();
+            world.gameOver();
             world.removeObject(this);
         }
         
